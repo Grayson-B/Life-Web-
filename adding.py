9aba87b5cd1ac9483bea96280db30490bbcc1d7f
Grayson-B <grayconbrundick@gmail.com>
2026-02-05 20:00:33 -0500

    We have made a python calculator

---
+++ adding.py
@@ -0,0 +1,31 @@
+first_number = int(input("enter your first number: ")) 
+Second_number = int(input("enter your second number: "))
+
+print(first_number - Second_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Problem Statement
+
+#Write a Python program that:
+
+#Asks the user for two numbers
+
+#Adds them together
+
+#Prints the sum
---
+++ testpython.py
@@ -0,0 +1,8 @@
+userinput = int(input("enter a number: "))
+if userinput % 2 == 0 and userinput != 0:
+    print("Even")
+elif userinput == 0:
+    print("is not even or odd")
+
+else:
+    print("Odd")
